ea6397ea207011bc0870232f89e53bba9c091ef3
Hugh Perkins <hughperkins@gmail.com>
2022-03-09 19:13:29 -0500

    python3

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,9 +8,10 @@ echo "ARG DEBIAN_FRONTEND=noninteractive"
 
 echo "RUN apt-get update"
 
-echo "RUN apt-get install -y gperf flex bison build-essential clang tcl-dev libboost-dev libfl-dev zlibc zlib1g zlib1g-dev perl git ccache libgoogle-perftools-dev numactl perl-doc python3"
+echo "RUN apt-get install -y gperf flex bison build-essential clang tcl-dev libboost-dev \
+    libfl-dev zlibc zlib1g zlib1g-dev perl git ccache libgoogle-perftools-dev numactl perl-doc python3"
 
-echo "RUN python -V"
+# echo "RUN python -V"
 
 echo "RUN python3 -V"
 
