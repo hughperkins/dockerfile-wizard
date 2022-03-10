6f0f218fd697ee8516b6fe62bf048eda6074d21d
Hugh Perkins <hughperkins@gmail.com>
2022-03-09 19:09:33 -0500

    non-interative

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,8 @@
 # echo "FROM cimg/base:stable-20.04"
 echo "FROM ubuntu:20.04"
 
+echo "ARG DEBIAN_FRONTEND=noninteractive"
+
 echo "RUN apt-get update"
 
 echo "RUN apt-get install -y gperf flex bison build-essential clang tcl-dev libboost-dev libfl-dev zlibc zlib1g zlib1g-dev perl git ccache libgoogle-perftools-dev numactl perl-doc python3"
